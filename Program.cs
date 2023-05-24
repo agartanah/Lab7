91732467ed4a7c21183a8749231907e7afbee190
agartanah <vyatin2017@mail.ru>
2023-05-24 20:39:53 +0300

    Implemented using external iterators to output the tree in ascending and descending order. Created a simple console application with the ability to create and sort a binary tree.

--- Program.cs
+++ Program.cs
@@ -1,21 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab7 {
   class Program {
+    delegate void IncreasingTree(List<int> Tree);
+    delegate void DescendingTree(List<int> Tree);
     static void Main(string[] args) {
-      BinaryTree<int> Tree = new BinaryTree<int>(100);
+      // Печатает элементы дерева в порядке возрастания
+      IncreasingTree Increase = (ListTree) => {
+        ListTree.Sort();
+        foreach (var item in ListTree) {
+          Console.WriteLine(item);
+        }
+      };
 
-      Tree.Add(123);
-      Tree.Add(1);
-      Tree.Add(12);
-      Tree.Add(3);
-      Tree.Add(100);
+      // Печатает элементы дерева в порядке увания
+      DescendingTree Descend = (ListTree) => {
+        ListTree.Sort();
+        ListTree.Reverse();
+        foreach (var Element in ListTree) {
+          Console.WriteLine(Element);
+        }
+      };
 
+      //
+      // Блок пользовательского меню
+      //
 
-      foreach (var item in Tree) {
-        Console.WriteLine(item);
+      Console.Write("Введите количество элементов в дереве: ");
+      int TreeElementCount = int.Parse(Console.ReadLine());
+
+      List<int> TreeList = new List<int>();
+
+      Console.WriteLine("Введите начальное значение: ");
+      int ElementBase = int.Parse(Console.ReadLine());
+
+      BinaryTree<int> Tree = new BinaryTree<int>(ElementBase); // Установка начального значения
+      TreeList.Add(ElementBase);
+
+      Console.WriteLine($"Введите поочерёдно {TreeElementCount - 1} элементов:\n");
+      for (int TreeElement = 0; TreeElement < TreeElementCount - 1; ++TreeElement) {
+        int Element = int.Parse(Console.ReadLine());
+        Tree.Add(Element);
+        TreeList.Add(Element);
       }
 
+      void SortTree() {
+        Console.Write("Как хотите отсортировать матрицу?\n\t1. По возрастанию\n\t2. По убыванию\nВведите желаемый пункт: ");
+        int UserChoice = int.Parse(Console.ReadLine());
+
+        switch (UserChoice) {
+          case 1:
+            Console.WriteLine("Дерево в порядке возрастания: \n");
+            Increase(TreeList);
+            break;
+          case 2:
+            Console.WriteLine("Дерево в порядке убывания: \n");
+            Descend(TreeList);
+            break;
+          default:
+            Console.WriteLine("Неверно введён пункт! Попробуйте ещё!");
+            SortTree();
+            break;
+        }
+      }
+
+      SortTree();
       Console.ReadKey();
     }
   }

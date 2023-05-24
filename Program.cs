7d047c65744a0ce116f32df9d5c5bf853ddc3b6c
agartanah <vyatin2017@mail.ru>
2023-05-24 20:33:20 +0300

    Tested the ability to iterate through the tree with foreach

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+
+namespace Lab7 {
+  class Program {
+    static void Main(string[] args) {
+      BinaryTree<int> Tree = new BinaryTree<int>(100);
+
+      Tree.Add(123);
+      Tree.Add(1);
+      Tree.Add(12);
+      Tree.Add(3);
+      Tree.Add(100);
+
+
+      foreach (var item in Tree) {
+        Console.WriteLine(item);
+      }
+
+      Console.ReadKey();
+    }
+  }
+}
